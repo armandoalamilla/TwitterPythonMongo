1088473176d7f2d81afdaddb11084ac430cfa215
Armando Aguiar Alamilla <31995672+armandoalamilla@users.noreply.github.com>
2019-11-19 13:09:55 -0600

    Create proyecto.py

---
+++ proyecto.py
@@ -0,0 +1,31 @@
+import tweepy
+import simplejson as json
+
+#override tweepy.StreamListener to add logic to on_status
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, data):
+        print("usuario",data._json['user']['name'])
+        print("contenido: ",data._json['text'])
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            #returning False in on_error disconnects the stream
+            return False
+
+consumer_key = 'AUpKZNqetVx6r4YoA7J4Hk2xU'
+consumer_secret = 'OjhhspAyItBOvE4nFQcTNmfE3CZg1tSnAvjraI4pX9BdIU5G5s'
+access_token = '238760734-sEiK8Mht5hpDG0elAIetI3shm5y7GB5av23wdWfP'
+access_token_secret = '2IMgpFk7UmzeyInaf6cl4zQx2QgVCAMN3PEoH87dk4TWc'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+
+myStream.filter(track=["mexico"])
+
+
+
